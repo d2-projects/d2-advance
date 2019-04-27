c2e98035ce032b7abc1eabfabd0ce884f0b084a0
CNine <cnine229@gmail.com>
2019-04-27 13:15:17 +0800

    [ui] add element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import { router, store } from './core'
 
 import 'normalize.css'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
+
 import './styles/index.styl'
 
 Vue.config.productionTip = false
