22d4b8ea83bdbc3db8cf59fde688b086807458e5
CNine <cnine229@gmail.com>
2019-04-27 23:48:17 +0800

    [ui] update welcome page

--- src/pages/home/welcome/index.vue
+++ src/pages/home/welcome/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="banner-container">
-    <h2>Welcome to D2 Advance</h2>
+    <h2>
+      <span style="color: #409eff">D2</span
+      ><span style="color: #35495e">Advance</span>
+    </h2>
     <p>
       Advance, convention first, colourful front-end practice. be inspired by
       D2Admin :)
