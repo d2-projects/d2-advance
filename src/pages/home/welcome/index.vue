587067f6665916a6b006012898f53f3a685c9d4a
CNine <cnine229@gmail.com>
2019-04-27 16:53:46 +0800

    [tech] join d2-projects

--- src/pages/home/welcome/index.vue
+++ src/pages/home/welcome/index.vue
@@ -1,9 +1,70 @@
 <template>
   <div class="banner-container">
-    <h2>Welcome to D2Advance</h2>
+    <h2>Welcome to D2 Advance</h2>
     <p>
       Advance, convention first, colourful front-end practice. be inspired by
       D2Admin :)
     </p>
+    <div class="badge">
+      <p align="center">
+        <a
+          ><img
+            src="https://img.shields.io/github/release/d2-projects/d2-advance.svg"
+        /></a>
+        <a
+          ><img
+            src="https://img.shields.io/github/forks/d2-projects/d2-advance.svg"
+        /></a>
+        <a
+          ><img
+            src="https://img.shields.io/github/stars/d2-projects/d2-advance.svg"
+        /></a>
+      </p>
+      <p align="center">
+        <a
+          ><img
+            src="https://img.shields.io/github/issues/d2-projects/d2-advance.svg"
+        /></a>
+        <a
+          ><img
+            src="https://img.shields.io/github/issues-closed/d2-projects/d2-advance.svg"
+        /></a>
+        <a
+          ><img
+            src="https://img.shields.io/github/issues-pr/d2-projects/d2-advance.svg"
+        /></a>
+        <a
+          ><img
+            src="https://img.shields.io/github/issues-pr-closed/d2-projects/d2-advance.svg"
+        /></a>
+      </p>
+      <p align="center">
+        <a href="https://www.travis-ci.org/d2-projects/d2-advance"
+          ><img
+            src="https://www.travis-ci.org/d2-projects/d2-advance.svg?branch=master"
+        /></a>
+        <a
+          ><img
+            src="https://img.shields.io/github/last-commit/d2-projects/d2-advance.svg"
+        /></a>
+        <a
+          ><img
+            src="https://img.shields.io/badge/code_style-standard-brightgreen.svg"
+        /></a>
+      </p>
+    </div>
   </div>
 </template>
+
+<style lang="stylus" scoped>
+.badge
+  margin-bottom 20px
+  p
+    margin 0px
+    margin-bottom 2px
+    &:last-child
+      margin-bottom 0px
+    a
+      display inline-block
+      margin 0px 2px
+</style>
