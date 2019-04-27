e042d2c9213c3211a8fcbeeee1e56ac2b97cda1b
CNine <cnine229@gmail.com>
2019-04-27 17:12:46 +0800

    [ui] split RepoBadge

---
+++ src/pages/home/welcome/components/RepoBadge.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="badge">
+    <p align="center">
+      <a
+        ><img
+          src="https://img.shields.io/github/release/d2-projects/d2-advance.svg"
+      /></a>
+      <a
+        ><img
+          src="https://img.shields.io/github/forks/d2-projects/d2-advance.svg"
+      /></a>
+      <a
+        ><img
+          src="https://img.shields.io/github/stars/d2-projects/d2-advance.svg"
+      /></a>
+    </p>
+    <p align="center">
+      <a
+        ><img
+          src="https://img.shields.io/github/issues/d2-projects/d2-advance.svg"
+      /></a>
+      <a
+        ><img
+          src="https://img.shields.io/github/issues-closed/d2-projects/d2-advance.svg"
+      /></a>
+      <a
+        ><img
+          src="https://img.shields.io/github/issues-pr/d2-projects/d2-advance.svg"
+      /></a>
+      <a
+        ><img
+          src="https://img.shields.io/github/issues-pr-closed/d2-projects/d2-advance.svg"
+      /></a>
+    </p>
+    <p align="center">
+      <a href="https://www.travis-ci.org/d2-projects/d2-advance"
+        ><img
+          src="https://www.travis-ci.org/d2-projects/d2-advance.svg?branch=master"
+      /></a>
+      <a
+        ><img
+          src="https://img.shields.io/github/last-commit/d2-projects/d2-advance.svg"
+      /></a>
+      <a
+        ><img
+          src="https://img.shields.io/badge/code_style-standard-brightgreen.svg"
+      /></a>
+    </p>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.badge
+  margin-bottom 20px
+  p
+    margin 0px
+    margin-bottom 2px
+    &:last-child
+      margin-bottom 0px
+    a
+      display inline-block
+      margin 0px 2px
+</style>
--- src/pages/home/welcome/index.vue
+++ src/pages/home/welcome/index.vue
@@ -5,66 +5,16 @@
       Advance, convention first, colourful front-end practice. be inspired by
       D2Admin :)
     </p>
-    <div class="badge">
-      <p align="center">
-        <a
-          ><img
-            src="https://img.shields.io/github/release/d2-projects/d2-advance.svg"
-        /></a>
-        <a
-          ><img
-            src="https://img.shields.io/github/forks/d2-projects/d2-advance.svg"
-        /></a>
-        <a
-          ><img
-            src="https://img.shields.io/github/stars/d2-projects/d2-advance.svg"
-        /></a>
-      </p>
-      <p align="center">
-        <a
-          ><img
-            src="https://img.shields.io/github/issues/d2-projects/d2-advance.svg"
-        /></a>
-        <a
-          ><img
-            src="https://img.shields.io/github/issues-closed/d2-projects/d2-advance.svg"
-        /></a>
-        <a
-          ><img
-            src="https://img.shields.io/github/issues-pr/d2-projects/d2-advance.svg"
-        /></a>
-        <a
-          ><img
-            src="https://img.shields.io/github/issues-pr-closed/d2-projects/d2-advance.svg"
-        /></a>
-      </p>
-      <p align="center">
-        <a href="https://www.travis-ci.org/d2-projects/d2-advance"
-          ><img
-            src="https://www.travis-ci.org/d2-projects/d2-advance.svg?branch=master"
-        /></a>
-        <a
-          ><img
-            src="https://img.shields.io/github/last-commit/d2-projects/d2-advance.svg"
-        /></a>
-        <a
-          ><img
-            src="https://img.shields.io/badge/code_style-standard-brightgreen.svg"
-        /></a>
-      </p>
-    </div>
+    <repo-badge />
   </div>
 </template>
 
-<style lang="stylus" scoped>
-.badge
-  margin-bottom 20px
-  p
-    margin 0px
-    margin-bottom 2px
-    &:last-child
-      margin-bottom 0px
-    a
-      display inline-block
-      margin 0px 2px
-</style>
+<script>
+import RepoBadge from './components/RepoBadge'
+
+export default {
+  components: {
+    RepoBadge
+  }
+}
+</script>
