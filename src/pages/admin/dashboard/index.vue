5499eac833fcc21cb34fbc65ab7a19bebd16f079
CNine <cnine229@gmail.com>
2019-05-04 17:37:46 +0800

    [ui] remove useless code

--- src/pages/admin/dashboard/index.vue
+++ src/pages/admin/dashboard/index.vue
@@ -3,17 +3,3 @@
     <h2>Welcome to the dashboard</h2>
   </div>
 </template>
-
-<script>
-import { mapActions } from 'vuex'
-
-export default {
-  methods: {
-    ...mapActions(['logout']),
-    async handleLogout() {
-      await this.logout()
-      this.$router.push('/login')
-    }
-  }
-}
-</script>
