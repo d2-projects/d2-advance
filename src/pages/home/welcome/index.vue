51effa6b3d0dc3056cb80695624fb0366a1fa822
CNine <cnine229@gmail.com>
2019-05-04 13:44:44 +0800

    [ui] improve welcome page

--- src/pages/home/welcome/index.vue
+++ src/pages/home/welcome/index.vue
@@ -3,8 +3,10 @@
     <h2>
       <span style="color: #409eff">D2</span
       ><span style="color: #35495e">Advance</span>
-      <span style="color: #c0c4cc;">@{{ version }}</span>
     </h2>
+    <p>
+      <span style="color: #c0c4cc; font-weight: 100;">@{{ version }}</span>
+    </p>
     <p>
       Advance, convention first, colourful front-end practice. be inspired by
       D2Admin :)
