cae4b6e134935ac87d2ab6aee9997ac250ab5cb6
CNine <cnine229@gmail.com>
2019-05-12 23:19:35 +0800

    [router] update collection demo

---
+++ src/pages/admin/food/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner-container" :key="food">
+    <h2>{{ food }}</h2>
+  </div>
+</template>
+
+<script>
+import { get } from 'lodash'
+
+export default {
+  computed: {
+    food() {
+      return get(this, '$route.params.food')
+    }
+  }
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,5 @@
 import fast from './fast'
 
-/* Fast */
 const routes = fast({
   [['/', 'home']]: {
     'welcome': 'home/welcome',
@@ -8,52 +7,10 @@ const routes = fast({
   },
   [['/admin', 'admin']]: {
     'dashboard': 'admin/dashboard',
-    'collection/milk-tea': '404',
-    'collection/ice-drink': '404',
-    'collection/apple': '404',
-    'collection/cherry': '404'
+    'collection/:food': 'admin/food'
   },
   '/404': '404',
   '*!redirect': '/404'
 })
-
-// OR
-
-/* Normal */
-/*
-const routes = [
-  {
-    path: '/',
-    redirect: '/welcome',
-    component: () => import('../pages/home'),
-    children: [
-      {
-        path: 'welcome',
-        component: () => import('../pages/home/welcome')
-      },
-      {
-        path: 'login',
-        component: () => import('../pages/home/login')
-      }
-    ]
-  },
-  {
-    path: '/admin',
-    redirect: '/admin/dashboard',
-    component: () => import('../pages/admin'),
-    children: [
-      {
-        path: 'dashboard',
-        component: () => import('../pages/admin/dashboard')
-      }
-    ]
-  },
-  {
-    path: '/404',
-    component: () => import('../pages/404')
-  },
-  { path: '*', redirect: '/404' }
-]
-*/
 
 export default routes
