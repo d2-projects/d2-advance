e70df0e8976930922a602563d07fd8adcd294d13
CNine <cnine229@gmail.com>
2019-05-18 12:07:58 +0800

    [ui] update collection demo

--- src/pages/admin/food/index.vue
+++ src/pages/admin/food/index.vue
@@ -1,14 +1,20 @@
 <template>
+  <!-- for router-view refresh -->
   <div :key="food" class="banner-container">
-    <h2>{{ food }}</h2>
+    <div style="font-size: 128px"><i :class="'el-icon-' + food" /></div>
+    <h2>{{ foodName }}</h2>
+    <h1>Collection demo</h1>
   </div>
 </template>
 
 <script>
-import { get } from 'lodash'
+import { get, startCase } from 'lodash'
 
 export default {
   computed: {
+    foodName() {
+      return startCase(this.food)
+    },
     food() {
       return get(this, '$route.params.food')
     }
