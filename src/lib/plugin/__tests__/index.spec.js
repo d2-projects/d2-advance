9c5554a854de9e9a4cce475cef00364a1e5af129
CNine <cnine229@gmail.com>
2019-07-30 21:59:27 +0800

    [test] improve test case for plugin core

--- src/lib/plugin/__tests__/index.spec.js
+++ src/lib/plugin/__tests__/index.spec.js
@@ -7,19 +7,35 @@ describe('Plugin system', () => {
     return module
   }
 
-  describe('Autowired plugin instance from config', () => {
+  describe('Autowired plugin instance from configin kebab path plugin', () => {
     it('should return myPlugin instance', () => {
       const myPlugin = jest.fn(() => 'myPluginInstance')
-      expect(
-        autowired(
-          mockRequireContextFromObject({
-            './myPlugin/index.js': myPlugin
-          }),
-          {
-            myPlugin: { option: 'foo' }
-          }
-        )
-      ).toEqual({
+      const config = { myPlugin: { option: 'foo' } }
+      const plugins = { './myPlugin/index.js': myPlugin }
+
+      expect(autowired(mockRequireContextFromObject(plugins), config)).toEqual({
+        myPlugin: 'myPluginInstance'
+      })
+      expect(myPlugin.mock.calls[0][0]).toEqual({ option: 'foo' })
+    })
+
+    it('should return myPlugin instance in kebab path plugin', () => {
+      const myPlugin = jest.fn(() => 'myPluginInstance')
+      const config = { myPlugin: { option: 'foo' } }
+      const plugins = { './my-plugin/index.js': myPlugin }
+
+      expect(autowired(mockRequireContextFromObject(plugins), config)).toEqual({
+        myPlugin: 'myPluginInstance'
+      })
+      expect(myPlugin.mock.calls[0][0]).toEqual({ option: 'foo' })
+    })
+
+    it('should return myPlugin instance in snake path plugin', () => {
+      const myPlugin = jest.fn(() => 'myPluginInstance')
+      const config = { myPlugin: { option: 'foo' } }
+      const plugins = { './my_plugin/index.js': myPlugin }
+
+      expect(autowired(mockRequireContextFromObject(plugins), config)).toEqual({
         myPlugin: 'myPluginInstance'
       })
       expect(myPlugin.mock.calls[0][0]).toEqual({ option: 'foo' })
