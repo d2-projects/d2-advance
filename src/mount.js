57856f25f15344581f342eca4d3e8715d7ee9428
CNine <cnine229@gmail.com>
2019-12-20 22:59:28 +0800

    feat(ui): add element-ui

--- src/mount.js
+++ src/mount.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.use(ElementUI)
 
 export default {
   mount() {
