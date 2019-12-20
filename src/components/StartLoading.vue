0a920aa69b5fa5d5645454421d3db94f229a9263
CNine <cnine229@gmail.com>
2019-12-20 20:37:59 +0800

    feat(loading): add start-loading with epic spinner, use VUE_APP_START_LOADING_DEBUG to debug it
    move main app logic of mount to mount.js. add start-loading view, and VUE_APP_START_LOADING_DEBUG to
    debug it.

---
+++ src/components/StartLoading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="start-loading">
+    <atom-spinner :animation-duration="1000" :size="60" :color="'#ff1d5e'" />
+  </div>
+</template>
+
+<script>
+import AtomSpinner from 'epic-spinners/src/components/lib/AtomSpinner'
+
+export default {
+  components: {
+    AtomSpinner
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.start-loading
+  text-align center
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
-import './plugins/pwa/registerServiceWorker'
-import router from './router'
-import store from './store'
+import StartLoading from '@/components/StartLoading.vue'
 
-Vue.config.productionTip = false
+const MIN_LOADING_TIME = 600
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+document.getElementById('app').appendChild(
+  new Vue({
+    render: h => h(StartLoading),
+    mounted() {
+      if (process.env.VUE_APP_START_LOADING_DEBUG === 'on') {
+        console.warn('VUE_APP_START_LOADING_DEBUG is turn on')
+        return
+      }
+
+      const startTime = new Date().getTime()
+
+      import('./mount')
+        .then(module => {
+          const loadingTime = new Date().getTime() - startTime
+          if (loadingTime < MIN_LOADING_TIME) {
+            setTimeout(() => {
+              !(module.default || module).mount()
+            }, MIN_LOADING_TIME - loadingTime)
+          }
+        })
+        .catch(error => {
+          console.error('Ops!', error)
+        })
+    }
+  }).$mount().$el
+)
