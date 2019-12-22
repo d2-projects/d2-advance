a02c52cb88918b97e13fb7d161e9aa13be7fdb8a
CNine <cnine229@gmail.com>
2019-12-22 23:15:47 +0800

    improvement: fade transition for app mounted

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <transition name="fade">
+    <div id="app" v-show="show">
+      <router-view />
+    </div>
+  </transition>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  mounted() {
+    this.show = true
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.fade
+  &-enter-active
+  &-leave-active
+    transition opacity .5s
+  &-enter
+  &-leave-to
+    opacity 0
+</style>
