e676fb524caf6cceefb850e55c13e27e24028cea
CNine <cnine229@gmail.com>
2019-12-24 14:28:41 +0800

    feat: d2-vue-application
    add d2-vue-application to integrate the boot process

--- src/start.js
+++ src/start.js
@@ -1,18 +1,63 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import routes from './routes'
+import RoutingGuards from './lib/routing-guards'
+import { D2VueApplication } from './lib/core'
 
+Vue.use(Vuex)
+Vue.use(VueRouter)
 Vue.use(ElementUI)
 
-export default {
-  start() {
-    new Vue({
-      router,
+class MyApplication extends D2VueApplication {
+  constructor() {
+    super()
+  }
+
+  createStore() {
+    return new Vuex.Store({
+      state: {},
+      mutations: {},
+      actions: {},
+      modules: {}
+    })
+  }
+
+  createRouter() {
+    const router = new VueRouter({ routes })
+
+    router.beforeEach(
+      new RoutingGuards()
+        .use(this.firstRoutingMiddleware())
+        .use(async () => {
+          await new Promise(resolve => setTimeout(resolve, 2000))
+        })
+        .callback()
+    )
+
+    return router
+  }
+
+  createVM({ store, router }) {
+    return new Vue({
       store,
+      router,
       render: h => h(App)
-    }).$mount('#app')
+    })
+  }
+
+  mount(vm) {
+    vm.$mount('#app')
+  }
+}
+
+export default {
+  app: null,
+  start(payload = {}) {
+    this.app = new MyApplication()
+    this.app.start(payload)
   }
 }
