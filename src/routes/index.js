4478d73d04142601cc015d985d2e97df1fc8416c
CNine <cnine229@gmail.com>
2019-12-28 12:35:00 +0800

    feat(admin): add component-demo pages, move page-container demo from dashboard to compoennt-demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,48 +12,13 @@ const routes = [
     children: [
       {
         path: 'dashboard',
-        name: 'dashboard',
+        name: 'admin-dashboard',
         component: () => import('../views/admin/dashboard')
       },
       {
-        path: 'submenu',
-        name: 'submenu',
-        component: () => import('../views/admin/dashboard')
-      },
-      {
-        path: 'submenu/foo',
-        name: 'submenu/foo',
-        component: () => import('../views/admin/dashboard')
-      },
-      {
-        path: 'submenu/bar',
-        name: 'submenu/bar',
-        component: () => import('../views/admin/dashboard')
-      },
-      {
-        path: 'submenu/drink/milk-tea',
-        name: 'submenu/drink/milk-tea',
-        component: () => import('../views/admin/dashboard')
-      },
-      {
-        path: 'submenu/drink/coffee',
-        name: 'submenu/drink/coffee',
-        component: () => import('../views/admin/dashboard')
-      },
-      {
-        path: 'submenu/fruits',
-        name: 'submenu/fruits',
-        component: () => import('../views/admin/dashboard')
-      },
-      {
-        path: 'submenu/fruits/apple',
-        name: 'submenu/fruits/apple',
-        component: () => import('../views/admin/dashboard')
-      },
-      {
-        path: 'submenu/fruits/cherry',
-        name: 'submenu/fruits/cherry',
-        component: () => import('../views/admin/dashboard')
+        path: 'component-demo/page-container',
+        name: 'admin-component-demo-page-container',
+        component: () => import('../views/admin/component-demo/page-container')
       }
     ]
   }
