c66b11f429165c6b42675f48fa90bc4d7e46387f
CNine <cnine229@gmail.com>
2020-01-07 15:09:47 +0800

    improvement(i18n): i18n demo that change language

---
+++ src/locales/index.js
@@ -0,0 +1,4 @@
+export const langs = [
+  { label: 'English', value: 'en' },
+  { label: '简体中文', value: 'zh-CN' }
+]
--- src/views/admin/development/index.vue
+++ src/views/admin/development/index.vue
@@ -9,6 +9,15 @@
 
     <el-divider />
 
+    <h2>i18n</h2>
+    <el-radio-group v-model="$i18n.locale">
+      <el-radio v-for="item in langs" :key="item.value" :label="item.value">{{
+        item.label
+      }}</el-radio>
+    </el-radio-group>
+
+    <el-divider />
+
     <h2>process.env</h2>
     <pre>{{ env }}</pre>
   </page-container>
@@ -19,12 +28,14 @@ import { container } from '@/views/admin/$index/components/mixins'
 import { values } from 'lodash'
 import { flags } from '@/store/modules/admin/mixins'
 import { FLAGS } from '@/constants/flags'
+import { langs } from '@/locales'
 
 export default {
   mixins: [container, flags],
 
   data() {
     return {
+      langs,
       env: process.env
     }
   },
