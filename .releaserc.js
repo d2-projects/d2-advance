8fbc4d94a7114febe618259b3792c2684a17e952
CNine <cnine229@gmail.com>
2020-01-13 10:02:37 +0800

    chore(release): set npmPublish to false for semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -11,7 +11,7 @@ module.exports = {
           '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.'
       }
     ],
-    '@semantic-release/npm',
+    ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/github',
     [
       '@semantic-release/git',
