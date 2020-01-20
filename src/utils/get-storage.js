e58c808a72b4bbc198c5be701c50e04127c7317b
CNine <cnine229@gmail.com>
2020-01-20 13:51:14 +0800

    improvement(storage): add LocalStorage prefix key with package info

--- src/utils/get-storage.js
+++ src/utils/get-storage.js
@@ -1,11 +1,16 @@
 import low from 'lowdb'
 import LocalStorageAdapters from 'lowdb/adapters/LocalStorage'
+import packageInfo from '../../package.json'
 
 const storages = {}
 
 export default (namespace = 'default', { defaults } = {}) => {
   if (!storages[namespace]) {
-    storages[namespace] = low(new LocalStorageAdapters(namespace))
+    storages[namespace] = low(
+      new LocalStorageAdapters(
+        `${packageInfo.name}__${packageInfo.version}__${namespace}`
+      )
+    )
   }
 
   const db = storages[namespace]
