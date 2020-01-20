9928eee48726d1c977695d0c8bdb4c196feb8365
CNine <cnine229@gmail.com>
2020-01-20 12:59:07 +0800

    feat(admin): persist basic preferences

--- src/store/modules/admin/index.js
+++ src/store/modules/admin/index.js
@@ -1,30 +1,43 @@
 import { getField, updateField } from 'vuex-map-fields'
 import { flattenMenuItemOfAdmin } from './utils'
 import { createMenu } from './menu'
+import { pick, keys } from 'lodash'
+import getStorage from '@/utils/get-storage'
 
 export const namespace = 'admin'
 
-const init = _app => {
+const init = app => {
   const menu = createMenu()
   const tabOptions = flattenMenuItemOfAdmin(menu)
+
+  const defaults = {
+    showTabs: true,
+    tabOpened: [tabOptions[0]],
+    asideCollapse: false,
+    asideTransition: true,
+    pageTransition: true,
+    showSourceLink: true
+  }
+
+  const storage = getStorage(namespace, { defaults })
+  const watchValues = state => pick(state, keys(defaults))
+  app.on('done:createStore', ({ store }) => {
+    store.watch(
+      rootState => watchValues(rootState[namespace]),
+      values => storage.setState(values).write()
+    )
+  })
 
   return {
     namespaced: true,
     state: {
-      showTabs: true,
-      tabOpened: [tabOptions[0]],
       tabOptions,
-
-      asideCollapse: false,
-      asideTransition: true,
-      pageTransition: true,
-
-      showSourceLink: true,
-
+      ...storage.getState(),
       menu
     },
     getters: {
       getField,
+      watchValues,
       menu(state) {
         return state.menu
       }
