14b228ba2a9395aea229679c81f1e200e6b94d29
CNine <cnine229@gmail.com>
2020-02-01 10:45:25 +0800

    refactor(start): all in root vm

--- src/start.js
+++ src/start.js
@@ -10,52 +10,47 @@ Vue.config.productionTip = false
 
 export const i18n = createI18n()
 
-const startApp = async failHandler => {
-  const MIN_LOADING_TIME = process.env.VUE_APP_MIN_LOADING_TIME || 800
-
-  if (process.env.VUE_APP_START_LOADING_DEBUG === 'on') {
-    // eslint-disable-next-line no-console
-    console.warn('VUE_APP_START_LOADING_DEBUG is turn on')
-    return
-  }
-
-  const startTime = new Date().getTime()
-  const module = await import(/* webpackChunkName: "main" */ './main')
-  const start = () => !(module.default || module).ready(failHandler)
-  const loadingTime = new Date().getTime() - startTime
-  loadingTime < MIN_LOADING_TIME
-    ? setTimeout(start, MIN_LOADING_TIME - loadingTime)
-    : start()
-}
-
-document.getElementById('app').appendChild(
-  new Vue({
-    i18n,
-    data: { error: null },
-    render(h) {
-      return h(StartLoading, { props: { error: this.error } })
-    },
-    beforeCreate() {
-      const lang = getStorage()
-        .get('lang')
-        .value()
-      if (langs.find(item => item.value === lang)) {
-        this.$i18n.locale = lang
-      }
-    },
-    async mounted() {
-      try {
-        await startApp(this)
-      } catch (error) {
-        this.fail(error)
-      }
-    },
-    methods: {
-      fail(error) {
-        this.error = error
-        // eslint-disable-next-line no-console
-        console.error(error)
-      }
+export const vm = new Vue({
+  i18n,
+  data: { error: null },
+  beforeCreate() {
+    const lang = getStorage()
+      .get('lang')
+      .value()
+    if (langs.find(item => item.value === lang)) {
+      this.$i18n.locale = lang
     }
-  }).$mount().$el
-)
+  },
+  async mounted() {
+    try {
+      const MIN_LOADING_TIME = process.env.VUE_APP_MIN_LOADING_TIME || 800
+      if (process.env.VUE_APP_START_LOADING_DEBUG === 'on') {
+        // eslint-disable-next-line no-console
+        console.warn('VUE_APP_START_LOADING_DEBUG is turn on')
+        return
+      }
+
+      const startTime = new Date().getTime()
+      const module = await import(/* webpackChunkName: "main" */ './main')
+      const start = () => !(module.default || module).ready(this)
+      const loadingTime = new Date().getTime() - startTime
+      loadingTime < MIN_LOADING_TIME
+        ? setTimeout(start, MIN_LOADING_TIME - loadingTime)
+        : start()
+    } catch (error) {
+      this.fail(error)
+    }
+  },
+  methods: {
+    fail(error) {
+      this.error = error
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+  },
+  render(h) {
+    return h(StartLoading, { props: { error: this.error } })
+  }
+})
+
+document.getElementById('app').appendChild(vm.$mount().$el)
