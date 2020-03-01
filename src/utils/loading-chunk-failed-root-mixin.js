89e66abb43cfc768eef60678efc7a6fd141f6a1a
CNine <cnine229@gmail.com>
2020-03-01 17:17:22 +0800

    fix: regexp for loading chunk failed

--- src/utils/loading-chunk-failed-root-mixin.js
+++ src/utils/loading-chunk-failed-root-mixin.js
@@ -1,7 +1,7 @@
 export default app => ({
   mounted() {
     this.$router.onError(error => {
-      if (/loading chunk \d* failed./i.test(error.message)) {
+      if (/loading chunk .* failed./i.test(error.message)) {
         app.emit('loadingChunkFailed', error)
         this.$alert(this.$t('loading-chunk-failed'), this.$t('ooops'), {
           confirmButtonText: this.$t('reload'),
