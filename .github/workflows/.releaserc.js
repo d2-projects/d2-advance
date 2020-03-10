f9482971269cf1ee7f7ef7c90ff7d09008c4df31
CNine <cnine229@gmail.com>
2020-03-10 21:19:23 +0800

    chore(release): add more release types

--- .github/workflows/.releaserc.js
+++ .github/workflows/.releaserc.js
@@ -1,7 +1,26 @@
 module.exports = {
   branch: 'master',
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        'preset': 'angular',
+        'releaseRules': [
+          { breaking: true, release: 'major' },
+          { revert: true, release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'build', release: 'patch' },
+          { type: 'ci', release: 'patch' }
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
