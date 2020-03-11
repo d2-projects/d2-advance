4c8cbe646fcce70b071e6c9a35e39a178376b806
CNine <cnine229@gmail.com>
2020-03-11 22:52:53 +0800

    chore(eslint): add eslint recommended config, change vue essential to recommended config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended', // or be simple -> 'plugin:vue/essential'
+    '@vue/prettier'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
