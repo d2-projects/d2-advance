22e8b57f03506ab338079aafd86cce7ccf13f6ff
CNine <cnine229@gmail.com>
2020-03-21 23:58:26 +0800

    chore(jest): strict options in comments

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest'
+  // collectCoverageFrom: [
+  //   '<rootDir>/src/**/*.{js,jsx}',
+  //   '!**/node_modules/**',
+  //   '!**/vendor/**',
+  //   '!**/__tests__/*.{j,t}s?(x)',
+  //   '!**/tests/unit/**/*.spec.{j,t}s?(x)'
+  // ]
 }
