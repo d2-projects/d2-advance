c6748dd8489d538c88be0ed2c87d5a1149692b40
CNine <cnine229@gmail.com>
2020-08-12 14:43:21 +0800

    chore: add prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,18 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: "none",
+        quoteProps: "preserve",
+        arrowParens: "always",
+        endOfLine: "auto"
+      }
+    ]
   },
   overrides: [
     {
