ac4dfd5cfed0c6bd73cf8022e9a3f8c4dc067b98
CNine <cnine229@gmail.com>
2020-12-13 16:36:09 +0800

    build: support gzip and brotli default

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,12 @@ module.exports = {
       env: {
         jest: true
       }
+    },
+    {
+      files: ['./*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
     }
   ]
 };
