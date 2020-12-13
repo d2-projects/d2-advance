76ec14f16e19f76562db5b4448dd8dc4e764408d
CNine <cnine229@gmail.com>
2020-12-13 16:46:25 +0800

    chore: set dev server overlay only error

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,15 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin');
 module.exports = {
   publicPath: process.env.BASE_URL || './',
   productionSourceMap: process.env.VUE_APP_SOURCE_MAP === 'on',
+
+  devServer: {
+    // display console info on page
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
+
   chainWebpack(config) {
     // https://github.com/webpack-contrib/compression-webpack-plugin
     if (process.env.NODE_ENV === 'production') {
