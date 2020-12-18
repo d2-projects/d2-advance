9d453b9719fc670b022480aebf0c23232a5f259b
CNine <cnine229@gmail.com>
2020-12-18 13:07:05 +0800

    fix: admin layout sidebar toggle

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -110,7 +110,25 @@
         class="flex justify-between items-center py-4 px-6 bg-white shadow z-10"
       >
         <div class="flex items-center">
-          <!-- left place -->
+          <button
+            class="text-gray-500 focus:outline-none lg:hidden"
+            @click="sidebarOpen = true"
+          >
+            <svg
+              class="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 6H20M4 12H20M4 18H11"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
         </div>
 
         <div class="flex items-center">
