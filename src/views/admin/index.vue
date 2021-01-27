3f74f392685f5ecd9f30d334ac15792d5ba71bb4
CNine <cnine229@gmail.com>
2021-01-28 00:21:45 +0800

    perf: styles

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -24,7 +24,7 @@
       <!-- logo in sidebar -->
       <div class="flex flex-col items-center justify-center mt-8 select-none">
         <div class="flex items-center">
-          <img class="h-8 w-8" src="@/assets/logo.png" />
+          <!-- <img class="h-8 w-8" src="@/assets/logo.png" /> -->
           <span class="text-white text-2xl mx-2 font-semibold">Admin</span>
         </div>
         <p class="mt-2 text-sm text-gray-500 font-mono">An elegant dashboard</p>
@@ -43,65 +43,24 @@
 
       <!-- menu -->
       <nav class="mt-10 select-none">
-        <router-link
-          :to="$RouteMap.ADMIN_DASHBOARD"
-          class="
-            flex items-center py-2 px-6 h-14 text-gray-500
-            hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
-            transition-all
-          "
-          active-class="bg-gray-700 bg-opacity-25 text-gray-100"
-        >
-          <svg
-            class="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
-            ></path>
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
-            ></path>
-          </svg>
-
-          <span class="mx-3">Dashboard</span>
-        </router-link>
-
-        <router-link
-          :to="$RouteMap.ADMIN_CRUD_DEMO"
-          class="
-            flex items-center py-2 px-6 h-14 text-gray-500
-            hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
-            transition-all
-          "
-          active-class="bg-gray-700 bg-opacity-25 text-gray-100"
-        >
-          <svg
-            class="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
-            ></path>
-          </svg>
-
-          <span class="mx-3">CRUD Demo</span>
-        </router-link>
+        <template v-for="({ label, to, icon }, index) in menu" :key="index">
+          <router-link v-slot="{ href, navigate, isActive }" :to="to" custom>
+            <a
+              :active="isActive"
+              :href="href"
+              :class="[
+                'flex items-center py-2 px-6 h-14 transition-all',
+                isActive
+                  ? 'bg-gray-700 bg-opacity-25 text-gray-100'
+                  : 'text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100'
+              ]"
+              @click="navigate"
+            >
+              <icon-park v-bind="icon" class="text-xl" />
+              <span class="mx-3">{{ label }}</span>
+            </a>
+          </router-link>
+        </template>
       </nav>
     </div>
 
@@ -246,9 +205,31 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import { RouteMap } from '@/constants/route';
+import { IconPark, IIconAllProps } from '@icon-park/vue-next/es/all';
 
 export default defineComponent({
+  components: { IconPark },
   setup() {
+    const menu = ref<
+      {
+        label: string;
+        to: string;
+        icon: IIconAllProps;
+      }[]
+    >([
+      {
+        label: 'Dashboard',
+        to: RouteMap.ADMIN_DASHBOARD,
+        icon: { type: 'Dashboard' }
+      },
+      {
+        label: 'CRUD Demo',
+        to: RouteMap.ADMIN_CRUD_DEMO,
+        icon: { type: 'ListView' }
+      }
+    ]);
+
     const sidebarOpen = ref(false);
     const notificationOpen = ref(false);
     const dropdownOpen = ref(false);
@@ -256,7 +237,8 @@ export default defineComponent({
     return {
       sidebarOpen,
       notificationOpen,
-      dropdownOpen
+      dropdownOpen,
+      menu
     };
   }
 });
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="min-h-screen flex flex-col justify-center">
-    <h1 class="text-center text-5xl space-x-4 mb-5 font-sans select-none">
+    <h1
+      class="text-center text-5xl space-x-4 mb-5 font-sans select-none font-bold"
+    >
       <span class="text-gray-700">D2</span>
       <span class="text-blue-500">Advance</span>
     </h1>
@@ -17,23 +19,40 @@
             :to="$RouteMap.ADMIN"
             class="
               text-white 
-              block px-6 py-5 rounded-md
-              bg-blue-500 space-y-1
+              p-5 rounded-md
+              bg-blue-500 space-x-4
               bg-gradient-to-br
               from-blue-400 to-blue-600
               hover:bg-blue-600 shadow-md hover:shadow-lg
               focus:outline-none focus:ring-4 focus:ring-blue-300
               transition-all
               w-72 min-h-full
+              flex flex-row items-center
             "
           >
-            <p class="font-semibold text-lg">Admin</p>
-            <p class="truncate">
-              An elegant dashboard
-            </p>
+            <form-one
+              class="text-blue-500 text-4xl p-2 rounded-xl bg-white shadow-lg"
+            />
+            <div>
+              <p class="font-semibold text-lg">Admin</p>
+              <p class="truncate">
+                An elegant dashboard
+              </p>
+            </div>
           </router-link>
         </li>
       </ul>
     </nav>
   </main>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { FormOne } from '@icon-park/vue-next';
+
+export default defineComponent({
+  components: {
+    FormOne
+  }
+});
+</script>
