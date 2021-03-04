522a47fa018f69d38755e4e23fa2e1afb7c0f8f4
CNine <cnine229@gmail.com>
2021-03-04 21:49:46 +0800

    chore: reset tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,7 @@ module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        d2sky: {
-          light: '#3884fe',
-          DEFAULT: '#2f74ff',
-          dark: '#1049bb',
-        },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
