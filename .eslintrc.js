939237dd7ed9ab3db3b1223b5a0f9fbddfb8b8d9
CNine <cnine229@gmail.com>
2021-03-04 22:21:50 +0800

    chore: remove jest and vuedx

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,13 +30,6 @@ module.exports = {
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: { jest: true },
-    },
-    {
       files: ['**/mock/*.*'],
       rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
