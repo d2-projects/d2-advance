bc231a3dc3e9fbeb76b447e0e7b60cb58284062f
CNine <cnine229@gmail.com>
2021-03-05 09:23:26 +0800

    chore: setup `@vitejs/plugin-legacy`

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, __dirname);
 
   return {
     base: env.VITE_BASE_URL,
-    plugins: [vue()],
+    plugins: [vue(), legacy()],
     resolve: {
       alias: {
         '@': '/src',
