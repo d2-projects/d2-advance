25bcf809e2347331ac928bfda98b55b7b361f450
CNine <cnine229@gmail.com>
2021-03-07 15:55:27 +0800

    fix(plugins/global): inject `$RouteMap` to vue global

--- src/plugins/global.ts
+++ src/plugins/global.ts
@@ -1,14 +1,16 @@
 import { App } from 'vue';
-import { RoutePath, RouteName } from '@/constants/route';
+import { RoutePath, RouteName, RouteMap } from '@/constants/route';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $RoutePath: typeof RoutePath;
     $RouteName: typeof RouteName;
+    $RouteMap: typeof RouteMap;
   }
 }
 
 export default (app: App): void => {
   app.config.globalProperties.$RoutePath = RoutePath;
   app.config.globalProperties.$RouteName = RouteName;
+  app.config.globalProperties.$RouteMap = RouteMap;
 };
