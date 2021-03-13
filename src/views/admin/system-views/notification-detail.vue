4ebfa5670bc4142bde40ff76696670cbcd59ce8b
CNine <cnine229@gmail.com>
2021-03-13 12:37:12 +0800

    chore: add default vscode config

--- src/views/admin/system-views/notification-detail.vue
+++ src/views/admin/system-views/notification-detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-6 py-8">
-    <h3 class="text-3xl font-medium">Notifications detail</h3>
+  <div class="container mx-auto py-8 px-6">
+    <h3 class="font-medium text-3xl">Notifications detail</h3>
   </div>
 </template>
